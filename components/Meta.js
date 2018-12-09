6aabe7dcb79dd17eb6442852a06e557b550c84e2
jf1016613 <james_fallon@bose.com>
2018-12-08 20:46:47 -0500

    added styled components, styled nav, styled page, themed

---
+++ components/Meta.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Meta = () => (
+	<Head>
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta charSet="utf-8" />
+		<link rel="shortcut icon" href="/static/favicon.png" />
+		<link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+		<title>SMOELT &amp; CO.</title>
+	</Head>
+);
+
+export default Meta;
