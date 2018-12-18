b0217d329e366cac3965bd61e6fe328fb679d3ae
jf1016613 <james_fallon@bose.com>
2018-12-18 05:12:18 -0500

    added optimistic response and cache updates to cart

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -27,9 +27,32 @@ class RemoveFromCart extends Component {
 	static propTypes = {
 		id: PropTypes.string.isRequired,
 	};
+	// this gets called as soon as we gat a response back that a mutation has been performed
+	update = (cache, payload) => {
+		console.log("running remove from cart update function");
+		// first read the cache
+		const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+		console.log(data);
+		// remove the item from the cart
+		const cartItemId = payload.data.removeFromCart.id;
+		data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+		// write it back to the cache
+		cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+	};
 	render() {
 		return (
-			<Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id: this.props.id }}>
+			<Mutation
+				update={this.update}
+				mutation={REMOVE_FROM_CART_MUTATION}
+				variables={{ id: this.props.id }}
+				optimisticResponse={{
+					__typename: "Mutation",
+					removeFromCart: {
+						__typename: "CartItem",
+						id: this.props.id,
+					},
+				}}
+			>
 				{(removeFromCart, { loading, error }) => (
 					<BigButton
 						onClick={() => {
